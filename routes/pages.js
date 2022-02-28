6d8e8e58edf2a155e652d49251be078996a9ad49
AyasSharqawe <92643395+AyasSharqawe@users.noreply.github.com>
2022-02-28 10:43:12 -0800

    Update pages.js

---
+++ routes/pages.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('signin');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/signin', (req, res) => {
+    res.render('signin');
+});
+
+router.get('/forgotPass', (req, res) => {
+    res.render('forgotPass');
+});
+
+router.get('/resetPass', (req, res) => {
+    res.render('resetPass');
+});
+module.exports = router;
